e4451aa1bceb4ceb114366596992050ce80df831
GTN-0906 <mascigaetano@gmail.com>
2023-03-21 21:27:22 +0100

    immobiliare: Add RecensioneCrontoller
    Change-Id: If4945cd8a99ab02bd26f25d84bd5b675b569208a

--- src/main/java/com/fanta4/immobiliare/controller/RecensioneController.java
+++ src/main/java/com/fanta4/immobiliare/controller/RecensioneController.java
@@ -1,4 +1,33 @@
 package com.fanta4.immobiliare.controller;
 
+import com.fanta4.immobiliare.persistence.model.Recensione;
+import com.fanta4.immobiliare.service.RecensioneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:4200")
+@RequestMapping("/api/recensioni")
+@RequiredArgsConstructor
 public class RecensioneController {
+
+    private final RecensioneService r;
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createRecensione(@RequestBody Recensione recesione) { r.createRecensione(recesione); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Recensione> findByID(@PathVariable Long id) { return r.getByID(id); }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deleteRecensione(@PathVariable Long id) { return r.deleteByID(id); }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Recensione> updateRecensione(@PathVariable Long id, Recensione recensione){
+        return r.updateRecensione(id, recensione);
+    }
 }
