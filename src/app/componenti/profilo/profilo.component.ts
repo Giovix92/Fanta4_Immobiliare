edde90feae359600d2e16aaacf5ecced9e67a5e1
Giovix92 <giovix92@giovix92.com>
2023-05-23 15:58:00 +0200

    frontend: profilo: Finish logic
    Change-Id: Ie24fc8b835f7748696b396c91950538ec5077c34

--- src/app/componenti/profilo/profilo.component.ts
+++ src/app/componenti/profilo/profilo.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
-import { FormsModule, FormControl, FormGroup } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
+import { ServiceService } from 'src/app/Service/service.service';
 
 @Component({
     selector: 'app-profilo',
@@ -10,25 +9,53 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ProfiloComponent implements OnInit {
     isEditing: boolean = false;
+    showPassword: boolean = false;
 
-    constructor(public auth: AuthService) { }
+    nomeValue: string = '';
+    cognomeValue: string = '';
+    telefonoValue: string = '';
+    emailValue: string = '';
+    passwordValue: string = '';
+    tipologiaValue: string = '';
+
+    constructor(public auth: AuthService, private service: ServiceService) { }
 
     ngOnInit(): void {
-        
+        this.nomeValue = this.auth.utenteCorrente.nome;
+        this.cognomeValue = this.auth.utenteCorrente.cognome;
+        this.telefonoValue = this.auth.utenteCorrente.telefono;
+        this.emailValue = this.auth.utenteCorrente.email;
+        this.passwordValue = this.auth.utenteCorrente.password;
+        this.tipologiaValue = this.auth.utenteCorrente.tipologia;
     }
 
     toggleEditing(): void {
         this.isEditing = !this.isEditing;
     }
 
-    sendToServer(): void {
-        // Implement your server logic here
-    }
-
-    // Inside your component class
-    showPassword: boolean = false;
-
     togglePasswordVisibility(): void {
         this.showPassword = !this.showPassword;
+    }
+
+    sendToServer(): void {
+        this.service.updateUtente(this.auth.utenteCorrente.id, {
+            nome: this.nomeValue,
+            cognome: this.cognomeValue,
+            id: this.auth.utenteCorrente.id,
+            telefono: this.telefonoValue,
+            email: this.emailValue,
+            password: this.passwordValue,
+            tipologia: this.tipologiaValue,
+            bannato: false,
+        }).subscribe(
+            () => {
+                this.auth.utenteCorrente.nome = this.nomeValue;
+                this.auth.utenteCorrente.cognome = this.cognomeValue;
+                this.auth.utenteCorrente.telefono = this.telefonoValue;
+                this.auth.utenteCorrente.email = this.emailValue;
+                this.auth.utenteCorrente.password = this.passwordValue;
+                this.auth.utenteCorrente.tipologia = this.tipologiaValue;
+            }
+        );
     }
 }
