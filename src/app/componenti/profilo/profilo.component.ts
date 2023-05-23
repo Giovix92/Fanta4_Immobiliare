b80524000de4cde8eabfb62fb6590e9fdd2901f8
Giovix92 <giovix92@giovix92.com>
2023-05-23 15:23:07 +0200

    frontend: profilo: Add show password icon + logic
    Change-Id: I17deebfa5f12ef937f248a3cc0ebe8778267227d

--- src/app/componenti/profilo/profilo.component.ts
+++ src/app/componenti/profilo/profilo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { FormsModule, FormControl, FormGroup } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-profilo',
@@ -22,5 +23,12 @@ export class ProfiloComponent implements OnInit {
 
     sendToServer(): void {
         // Implement your server logic here
+    }
+
+    // Inside your component class
+    showPassword: boolean = false;
+
+    togglePasswordVisibility(): void {
+        this.showPassword = !this.showPassword;
     }
 }
