9abcbbd1ca2bc99a0db492f6d6e4d07aff7745a1
Giovix92 <giovix92@giovix92.com>
2023-05-24 15:01:59 +0200

    frontend: dashboard: Fix mode initialization error
    * Mode was applied before the viewchild decorator had the chance to even assign the sidenav variable.
    * Solution: use AfterViewInit.
    
    Change-Id: I89d9fbee8188061a6a3aae2a0e7c3ef00ffe8441

--- src/app/componenti/dashboard/dashboard.component.ts
+++ src/app/componenti/dashboard/dashboard.component.ts
@@ -1,30 +1,29 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { BreakpointObserver, Breakpoints, MediaMatcher} from '@angular/cdk/layout';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints, MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
-import {AuthService} from "../../auth/auth.service";
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements AfterViewInit {
   isSidenavOpened: boolean = false;
   isMobile = false;
-
   @ViewChild('sidenav') sidenav!: MatSidenav;
   private mobileQuery: MediaQueryList;
-  constructor(private breakpointObserver: BreakpointObserver, private mediaMatcher: MediaMatcher, public auth: AuthService) {
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private mediaMatcher: MediaMatcher,
+    public auth: AuthService
+  ) {
     this.mobileQuery = mediaMatcher.matchMedia('(max-width: 600px)');
   }
 
-  ngOnInit() {
-    this.breakpointObserver.observe([
-      Breakpoints.Handset,
-      Breakpoints.TabletPortrait
-    ]).subscribe(result => {
+  ngAfterViewInit() {
+    this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.TabletPortrait]).subscribe(result => {
       this.isMobile = result.matches;
       this.sidenav.mode = this.isMobile ? 'over' : 'side';
       this.sidenav.close();
@@ -38,5 +37,4 @@ export class DashboardComponent implements OnInit {
   Mobile(): boolean {
     return this.mobileQuery.matches;
   }
-
 }
